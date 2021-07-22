7548814e1d823820a192812dd00b3e845bd0a5e5
Christopher Boyle <cboyle824@gmail.com>
2021-07-22 03:09:39 -0400

    starting fresh

--- advaced-forms/src/components/Form.js
+++ advaced-forms/src/components/Form.js
@@ -1,70 +1,9 @@
 import React from "react";
 
 export default function Form(props) {
-  const { values, change, disabled, submit, errors } = props;
-
-  const onChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    const valueToUse = type === "checkbox" ? checked : value;
-    change(name, valueToUse);
-  };
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    submit();
-  };
-
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <h2>Add User</h2>
-        <button disabled={disabled}>submit</button>
-        <div className="errors">
-          <div>{errors.username}</div>
-          <div>{errors.email}</div>
-          <div>{errors.password}</div>
-          <div>{errors.tos}</div>
-        </div>
-        <div className="form-group inputs">
-          <h4>General Informaion</h4>
-          <label>
-            UserName:
-            <input
-              type="text"
-              name="username"
-              onChange={onChange}
-              values={values.username}
-            />
-          </label>
-          <label>
-            E-Mail:
-            <input
-              type="email"
-              name="email"
-              onChange={onChange}
-              value={values.email}
-            />
-          </label>
-          <label>
-            Password:
-            <input
-              type="password"
-              name="password"
-              onChange={onChange}
-              value={values.password}
-            />
-          </label>
-          <label>
-            Terms of Service:
-            <input
-              type="checkbox"
-              name="tos"
-              onChange={onChange}
-              checked={values.tos}
-            />
-          </label>
-        </div>
-      </div>
+    <form>
+      <div></div>
     </form>
   );
 }
--- advaced-forms/src/validation/formSchema.js
+++ advaced-forms/src/validation/formSchema.js
@@ -1,21 +1,5 @@
 import * as yup from "yup";
 
-const formSchema = yup.object().shape({
-  username: yup
-    .string()
-    .trim()
-    .required("Username is required")
-    .min(3, "Username must be 3 characters long"),
-  email: yup
-    .string()
-    .email("Please enter a valid e-mail address")
-    .required("Email is required"),
-  password: yup
-    .string()
-    .trim()
-    .required("please enter your password")
-    .min(4, "Password must be at least 4 characters in length"),
-  tos: yup.boolean(),
-});
+const formSchema = yup.object().shape({});
 
 export default formSchema;

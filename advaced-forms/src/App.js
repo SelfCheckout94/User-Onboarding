49eeacc7b3f9e13d8cf7d8abb53149d51cad4aa0
Christopher Boyle <cboyle824@gmail.com>
2021-07-22 03:38:36 -0400

    basic form is made and the initial form values are being passed to Form.js

--- advaced-forms/src/App.js
+++ advaced-forms/src/App.js
@@ -9,10 +9,20 @@ import { reach } from "yup";
 import schema from "./validation/formSchema";
 import { v4 as uuidv4 } from "uuid";
 
+const initialFormValues = {
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+  tos: false,
+};
+
 function App() {
+  const [formValues, setFormValues] = useState(initialFormValues);
+  console.log(formValues);
   return (
     <div className="App">
-      <Form />
+      <Form values={formValues} />
     </div>
   );
 }
--- advaced-forms/src/components/Form.js
+++ advaced-forms/src/components/Form.js
@@ -1,9 +1,73 @@
 import React from "react";
 
 export default function Form(props) {
+  const { values } = props;
+
+  const onChange = (e) => {};
+
+  const onSubmit = (e) => {};
+
   return (
-    <form>
-      <div></div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <h2>Add User</h2>
+        <div className="errors">
+          <div>example name error</div>
+          <div>example email error</div>
+          <div>example password error</div>
+          <div>example ToS error</div>
+        </div>
+        <div className="inputs">
+          <label>
+            First Name:
+            <input
+              name="first name"
+              type="text"
+              value={values.first_name}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Last Name:
+            <input
+              name="last name"
+              type="text"
+              value={values.last_name}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            E-Mail:
+            <input
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={onChange}
+            />
+          </label>
+          <label>
+            By checking this box you agree to our Terms of Service
+            <input
+              name="tos"
+              type="checkbox"
+              value={values.tos}
+              onChange={onChange}
+            />
+          </label>
+        </div>
+        <div>
+          <button>Submit</button>
+        </div>
+      </div>
     </form>
   );
 }
